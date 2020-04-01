a01928255e001bc13f052661b1a24d0678cc77c9
dylan <dylan@192.168.1.15>
2020-04-01 21:48:40 +0100

    File Chooser update

--- Topic Modeller/src/com/assignment/FileChooser.java
+++ Topic Modeller/src/com/assignment/FileChooser.java
@@ -1,5 +1,11 @@
 package com.assignment;
 
+/*******
+ * ..
+ * Author: Dylan O'Connor
+ * Start Date: 30/03/2020
+ * End Date: ..
+ *******/
 import javax.swing.*;
 import java.awt.*;
 
@@ -14,16 +20,20 @@ public class FileChooser extends JFrame{
     public FileChooser (String myTitle) {
     	super(myTitle);
     	
+    	//Frame/Panel
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         searchButton = new JButton("Search Files!");
-
+        
+        //heading
         heading = new JLabel("Welcome to Topic Modeller!  Enter File names to check if they are related!");
         
+        //Labels
         fileLabel1 = new JLabel("File Name: ");
         fileLabel2 = new JLabel("File Name: ");
         fileLabel3 = new JLabel("File Name: ");
         
+        //Text Field Elements
         file1Text = new JTextField(20);
         file2Text = new JTextField(20);
         file3Text = new JTextField(20);
@@ -31,25 +41,33 @@ public class FileChooser extends JFrame{
         frame.getContentPane().add(BorderLayout.NORTH, heading);
         frame.getContentPane().add(BorderLayout.SOUTH, searchButton);
         
-
+        //Setting up gridlayout
         panel.setLayout(new GridBagLayout());
         frame.getContentPane().add(panel);
+        
+        //leftside elements
         GridBagConstraints left = new GridBagConstraints();
         left.anchor = GridBagConstraints.EAST;
+        
+        //right side elements
         GridBagConstraints right = new GridBagConstraints();
         right.weightx = 2.0;
         right.fill = GridBagConstraints.HORIZONTAL;
         right.gridwidth = GridBagConstraints.REMAINDER;
         
-        
+        //adding elements to gui
         panel.add(fileLabel1, left);
         panel.add(file1Text, right);
         panel.add(fileLabel2, left);
         panel.add(file2Text, right);
         panel.add(fileLabel3, left);
         panel.add(file3Text, right);
-
+        
+        //setting border around center so items are spread out
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+        
+        //sizing window to the default size
+        //so that elements fit on screen best
         frame.pack();
         frame.setVisible(true);
     }
