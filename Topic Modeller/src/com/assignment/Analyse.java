e21e503eb3f9167e955e062c64029677af1b1072
dylan <dylan@192.168.1.15>
2020-04-02 19:43:47 +0100

    Analyse Class added

---
+++ Topic Modeller/src/com/assignment/Analyse.java
@@ -0,0 +1,15 @@
+package com.assignment;
+
+public class Analyse extends FileChooser{
+
+	//default serial verision
+	private static final long serialVersionUID = 1L;
+
+	//attributes
+	
+	public Analyse(String myTitle) {
+		super(myTitle);
+		// TODO Auto-generated constructor stub
+	}
+
+}
