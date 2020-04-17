b4b3ab0e00e0ec4c5d9575be1995a52a654e8c16
dylan <dylan@192.168.1.15>
2020-04-17 23:16:53 +0100

    Overall Grade Added

--- Topic Modeller/src/com/assignment/Analyse.java
+++ Topic Modeller/src/com/assignment/Analyse.java
@@ -5,10 +5,11 @@
  ***/
 package com.assignment;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.swing.JTable;
 
-//import java.io.FileNotFoundException;
-//import java.util.HashMap;
 //import java.util.Iterator;
 //import java.util.Map;
 
@@ -16,14 +17,43 @@ public class Analyse {
 	
 	JTable table;
 	JTable table1;
-
-	public Analyse(JTable table, JTable table1){
+	HashMap<String , Integer> overLap = new HashMap<String, Integer>();
+	public Analyse(JTable table){
 		this.table = table;
-		this.table1 = table1;
-		
 	}
 	
-	public void getOverlap() {
+	public HashMap<String , Integer> getOverlap(JTable table) {
 		
+		for (int row = 0; row < table.getRowCount(); row++){
+			if(overLap.containsKey(table.getValueAt(row, 0))) {
+				int counter = overLap.get(table.getValueAt(row, 0))+ 1;
+				overLap.put(table.getValueAt(row, 0).toString(), counter);
+			}//end if
+			else {
+				overLap.put( table.getValueAt(row, 0).toString(), 1);
+			}//end else
+		}
+		
+		System.out.println("OverLap: "+overLap);
+		
+		return overLap;
+	}
+	
+	public float overLapPercentage(HashMap<String, Integer>overlap) {
+		
+		float counter=0;
+		float overlapPercent=0;
+		
+		for (Map.Entry<String, Integer> entry : overlap.entrySet()) {
+			if(entry.getValue() >= 2) {
+				counter = counter + 1;
+				System.out.println(counter);
+			}
+		}
+		
+		overlapPercent = ((counter / overlap.size())*100);
+		System.out.println(overlapPercent);
+		
+		return overlapPercent;
 	}
 }//end Analyse 
